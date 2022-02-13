2b8c2af6fe0dad4865a42932e4493cf3755db934
ChrisVn188 <chrisbui.dev@gmail.com>
2022-02-13 18:09:06 -0500

    Change style of the app

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 'use strict';
 
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
 
+const player = $('.player');
 const playList = $('.playlist');
-const dashboardImg = $('.dashboard__img-container');
+const imageContainer = $('.dashboard__img-container')
+const songImg = $('.dashboard__img')
+const songTitle = $('.dashboard__title')
+const playBtn = $('.dashboard__toggle-play-pause')
+const nextBtn = $('.dashboard__next-btn')
+const prevBtn = $('.dashboard__prev-btn')
+const replayBtn = $('.dashboard__replay-btn')
+const autoNextSongBtn = $('.dashboard__shuffle-btn')
+const timeSeekingSlider = $('.dashboard__slider')
+const audio = $('.dashboard__audio')
 
 const app = {
+    currentIndex:  0,
+    canPlayThrough: false,
+    canReplay: false,
     songs: [
         {
-            title:'Hoa Hai Duong',
-            singer:'Jack',
-            imgUrl:'./images/hoa-hai-duong.jpeg',
-            audio:'./audio/hoa-hai-duong.mp3'
+            title:'Roi Toi Luon',
+            singer:'Nal',
+            imgUrl:'./images/roi-toi-luon.jpeg',
+            audio:'./audio/roi-toi-luon.mp3'
         },
-    
+
         {
             title:'Thien Dang',
             singer:'Wowwy & Jolipoli',
             imgUrl:'./images/thien-dang.jpeg',
             audio:'./audio/thien-dang.mp3'
         },
-    
+
         {
-            title:'Roi Toi Luon',
-            singer:'Nal',
-            imgUrl:'./images/roi-toi-luon.jpeg',
-            audio:'./audio/roi-toi-luon.mp3'
+            title:'Hoa Hai Duong',
+            singer:'Jack',
+            imgUrl:'./images/hoa-hai-duong.jpeg',
+            audio:'./audio/hoa-hai-duong.mp3'
         },
     
         {
@@ -50,43 +63,129 @@ const app = {
             audio:'./audio/khue-moc-lang.mp3'
         }
     ],
-
-
+    
+    loadCurrentSong(){
+        const currentSong = this.songs[this.currentIndex]
+        songImg.src = currentSong.imgUrl
+        songTitle.textContent = currentSong.title
+        audio.src = currentSong.audio
+        audio.play()
+    },
 
     /****************** HANDLE EVENTS ******************/
-    handleScroll(){
-        let lastScrollTop = 0;
-
-        const initialImgWidth = dashboardImg.offsetWidth;
-        let currentImageWidth = initialImgWidth;
-        
-        document.addEventListener('scroll', function(){
-            const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
-
-            if (currentScrollTop > lastScrollTop) {
-                // down scroll code
-                currentImageWidth -= currentScrollTop - lastScrollTop;
-                
-            } else {
-                // up scroll code
-                currentImageWidth += lastScrollTop - currentScrollTop;
-            }
-
-            dashboardImg.style.width = `${currentImageWidth}px`
-            dashboardImg.style.opacity = currentImageWidth / initialImgWidth;
-            
-            // fix negative scroll offset in mobile devices
-            lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
-        })
-    },
-    
     handleEvents(){
         // handle scroll event on document
-        this.handleScroll()
+        const initialImgWidth = imageContainer.offsetWidth
+        document.addEventListener('scroll', function(){
+            const scrollTop = window.scrollY || document.documentElement.scrollTop
+            const newWidth = initialImgWidth - scrollTop
+
+            imageContainer.style.width = `${newWidth}px`
+            imageContainer.style.opacity = newWidth / initialImgWidth
+        })
+
+
+        // handle play and pause audio
+        playBtn.addEventListener('click', (e) => {
+            if (e.target.closest('.icon-play')) {
+                audio.play()
+            } else{    
+                audio.pause()
+            }   
+        })
+
+
+        // display play btn
+        audio.addEventListener('play', () => {
+            player.classList.add('playing')
+        })
+
+
+         // display pause btn
+         audio.addEventListener('pause', () => {
+            player.classList.remove('playing')
+        })
+
+
+        // handle time update
+        audio.addEventListener('timeupdate', () => {
+            if(audio.duration > 0){
+                timeSeekingSlider.value = audio.currentTime / audio.duration * 100
+            }
+        })
+
+
+        // handle slider value change
+        timeSeekingSlider.addEventListener('input', () => {
+            audio.currentTime = timeSeekingSlider.value * audio.duration / 100
+        })
+
+
+        // handle next song
+        nextBtn.addEventListener('click', () => {
+            if (this.currentIndex < this.songs.length - 1) {
+                this.currentIndex++
+            } else {
+                this.currentIndex = 0
+            }
+            this.loadCurrentSong()
+        })
+
+
+         // handle prev song
+        prevBtn.addEventListener('click', () => {
+            if (this.currentIndex === 0) {
+                this.currentIndex = this.songs.length - 1
+            } else {
+                this.currentIndex--
+            }
+
+            this.loadCurrentSong()
+        })
+
+        // handle replay
+        replayBtn.addEventListener('click', () => {
+            replayBtn.classList.toggle('active')
+            autoNextSongBtn.classList.remove('active')
+            if (this.canReplay) {
+                this.canReplay = false           
+            } else {
+                this.canReplay = true
+                this.canPlayThrough = false
+            }
+            audio.loop = this.canReplay
+        })
+
+        // handle playing next song automatically
+        autoNextSongBtn.addEventListener('click', () => {
+            autoNextSongBtn.classList.toggle('active')
+            replayBtn.classList.remove('active')
+            if (this.canPlayThrough) {
+                this.canPlayThrough = false           
+            } else {
+                this.canPlayThrough = true
+                this.canReplay = false
+                audio.loop = false
+            }
+        })
+
+        // handle audio ended
+        audio.addEventListener('ended', () => {
+            // if the current song is the last, go back to the first song
+            if (this.currentIndex < this.songs.length - 1  
+                && this.canPlayThrough) {   
+                this.currentIndex++
+            }
+            else {
+                this.currentIndex = 0
+            }
+            this.loadCurrentSong()
+        })
+
     },
 
-    /****************** RENDER ******************/
-    render(){
+    /****************** RENDER SONGS ******************/
+    renderSongs(){
 
         const htmls = this.songs.map(s => {
             return `
@@ -109,9 +208,13 @@ const app = {
 
     },
 
+    /****************** START ******************/
     start(){
+        this.renderSongs()
+
+        this.loadCurrentSong()
+
         this.handleEvents()
-        this.render()
     }
 }
 
